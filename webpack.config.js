95b0cc01c40e77f33750084f5069bc868429d94e
dachi <ar.19920223@gmail.com>
2016-04-17 16:47:21 +0900

    add development tools

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+/* eslint-disable */
+
+var path = require('path')
+var fs = require('fs')
+var webpack = require('webpack')
+
+var nodeModules = {};
+fs.readdirSync('node_modules').filter(function(x) {
+  return ['.bin'].indexOf(x) === -1;
+}).forEach(function(mod) {
+  nodeModules[mod] = 'commonjs ' + mod;
+});
+
+module.exports = {
+  target: 'node',
+  externals: nodeModules,
+  entry: {
+    bundle: path.join(__dirname, 'app', 'index.js')
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].js'
+  },
+  resolve: {
+    extensions: ['', '.js']
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loaders: ['babel']
+      },
+      {
+        test: /\.json$/,
+        exclude: /node_modules/,
+        loaders: 'json'
+      }
+    ]
+  },
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(true),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      output: {
+        comments: false
+      }
+    })
+  ]
+}
