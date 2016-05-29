9ef8ef4116dc5da0e18cb33ae047305428e42de4
dachi <ar.19920223@gmail.com>
2016-05-29 14:57:58 +0900

    fix memo api function bugs

--- app/teams.js
+++ app/teams.js
@@ -27,7 +27,7 @@ class Teams extends Resource {
    * get memos
    */
   searchMemo(q = '*', page = 1, perPage = 20) {
-    return this.axios.get(`/teams/${this.domain}/posts`, {q, page, perPage})
+    return this.axios.get(`/teams/${this.domain}/posts?q=${encodeURIComponent(q)}&page=${page}&per_page=${perPage}`)
   }
 
   /**
@@ -40,7 +40,7 @@ class Teams extends Resource {
   /**
    * update memo
    */
-  updateMemo(id, {title, body, draft = false, notice = true, tags = [], scope = 'everyone', groups = []}) {
+  updateMemo(id, {title, body, draft = false, notice = true, tags, scope, groups}) {
     return this.axios.patch(`/teams/${this.domain}/posts/${id}`, {title, body, draft, notice, tags, scope, groups})
   }
 
